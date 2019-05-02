d0fe26531a65a58d957d6c463c7f22d01bd5816a
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-05-02 13:49:00 -0600

    installed boot/reacstrap & ran npm build

---
+++ build/precache-manifest.2b268381a164b32e40db485d0b6f0c8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2d2c01ea31cdf700149faeca182532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a377089069d71212595",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "161c19924c4fb3a5aff8",
+    "url": "/static/css/main.ad264f80.chunk.css"
+  },
+  {
+    "revision": "0a377089069d71212595",
+    "url": "/static/js/2.562f4f74.chunk.js"
+  },
+  {
+    "revision": "161c19924c4fb3a5aff8",
+    "url": "/static/js/main.dc69606d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
